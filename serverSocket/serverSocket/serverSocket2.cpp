9b6b7cc473e0d64064519d0531fde116cb3609b1
Rohit Borse <codingwithrb@gmail.com>
2023-06-04 14:00:15 +0530

    Folder Add

---
+++ serverSocket/serverSocket/serverSocket2.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include<iostream>
+#include<WinSock.h>
+using namespace std;
+
+int main2()
+{
+	//Initilized WSA variables 
+	//Initilized Socket 
+	int sock = socket(AF_INET,SOCK_STREAM,);
+	//Bind socket with local port
+	//Listen incomming request and store in waiting queue
+
+		return 0;
+}
